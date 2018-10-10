fe15b606afa6f737f957f99a3b0447f4321abda8
20161104574 <a20161104574@admindeiMac-12.local>
2018-10-10 10:20:15 +0800

    f

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func equal(_ sender: Any) {
+    }
+    @IBAction func dot(_ sender: Any) {
+    }
+    @IBAction func zero(_ sender: Any) {
+    }
+    @IBAction func nine(_ sender: Any) {
+    }
+    @IBAction func eight(_ sender: Any) {
+    }
+    @IBAction func seven(_ sender: Any) {
+    }
+    @IBAction func six(_ sender: Any) {
+    }
+    @IBAction func five(_ sender: Any) {
+    }
+    @IBAction func four(_ sender: Any) {
+    }
+    @IBAction func three(_ sender: Any) {
+    }
+    @IBAction func two(_ sender: Any) {
+    }
+    @IBAction func one(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
