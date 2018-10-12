6430a36577e207d1d024c6c06a83006c0058b4f3
20161104574 <a20161104574@admindeiMac-12.local>
2018-10-12 17:24:47 +0800

    n
    N

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -9,34 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBAction func equal(_ sender: Any) {
+    var a=0
+    var b=0
+    var c=0
+    @IBAction func display(_ sender: Any) {
     }
+    @IBOutlet weak var output: UITextField!
+    @IBOutlet weak var te: UITextField!
+    
+    @IBAction func except(_ sender: Any) {
+        te.text=te.text! + "/"
+    }
+    @IBAction func ride(_ sender: Any) {
+        te.text=te.text! + "x"
+    }
+    @IBAction func reduce(_ sender: Any) {
+        te.text=te.text! + "-"
+    }
+    @IBAction func plus(_ sender: Any) {
+        if te.text == ""{
+            output.text="0"
+        }
+        else
+        {
+            te.text = te.text! + "+"
+        }
+        //te.text=te.text! + "+"
+    }
+    
     @IBAction func dot(_ sender: Any) {
+        te.text=te.text! + "."
     }
     @IBAction func zero(_ sender: Any) {
+        te.text=te.text! + "0"
     }
-    @IBAction func nine(_ sender: Any) {
-    }
+    
     @IBAction func eight(_ sender: Any) {
+        te.text=te.text! + "8"
     }
     @IBAction func seven(_ sender: Any) {
+        te.text=te.text! + "7"
     }
     @IBAction func six(_ sender: Any) {
+        te.text=te.text! + "6"
     }
     @IBAction func five(_ sender: Any) {
+        te.text=te.text! + "5"
     }
     @IBAction func four(_ sender: Any) {
+        te.text=te.text! + "4"
     }
     @IBAction func three(_ sender: Any) {
+        te.text=te.text! + "3"
     }
     @IBAction func two(_ sender: Any) {
+        te.text=te.text! + "2"
     }
     @IBAction func one(_ sender: Any) {
+        te.text=te.text! + "1"
     }
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
